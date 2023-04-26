80252d5f9c2775aab116d7ecc47a4d1e66c45cb2
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-04-26 15:59:55 +0700

    search added

--- index.js
+++ index.js
@@ -1,9 +1,55 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+
+app.use(cors())
+
+
+// Data
+let data = [
+    {
+      title: 'JS tutorials',
+      description: 'The best JavaScript tutorials in the galaxy!',
+      url: 'https://www.w3schools.com',
+      links: [
+        {
+          title: 'JS for Beginners',
+          url: 'https://www.w3schools.com/js',
+        },
+        {
+          title: 'JS for the Web',
+          url: 'https://www.w3schools.com/js',
+        },
+      ],
+    },
+  ]
+
+
+  //Functions 
+  function search(string) {
+    let results = data.filter(
+      (n) =>
+        n.url.toLowerCase().includes(string.toLowerCase()) ||
+        n.description.toLowerCase().includes(string.toLowerCase()) ||
+        n.title.toLowerCase().includes(string.toLowerCase())
+    )
+    return results
+  }
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to Google Search')
 })
+
+app.get('/results', (req, res) => {
+let searchResults = search(req.query.search)
+ 
+
+}) 
 
 
 
