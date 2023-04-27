9b4bb086b7cbbb8817ae6311654a88285d3d7ce0
salmankhawar <78558343+salmankhawar@users.noreply.github.com>
2023-04-27 16:50:36 +0700

    Finished

--- index.js
+++ index.js
@@ -1,59 +1,33 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient} = require('mongodb')
+const client = new MongoClient(
+  'mongodb+srv://khawarsalman:Ozv15RxPy3B2BENq@google-search.od5xihm.mongodb.net/google-search'
+)
+const db = client.db('google-search')
+const Results = db.collection('Results')
 const app = express()
 
 
 app.use(cors())
 
 
-// Data
-let data = [
-    {
-      title: 'JS tutorials',
-      description: 'The best JavaScript tutorials in the galaxy!',
-      url: 'https://www.w3schools.com',
-      links: [
-        {
-          title: 'JS for Beginners',
-          url: 'https://www.w3schools.com/js',
-        },
-        {
-          title: 'JS for the Web',
-          url: 'https://www.w3schools.com/js',
-        },
-      ],
-    },
-  ]
-
-
-  //Functions 
-  function search(string) {
-    if (!string) {
-        return []
-        }
-    let results = data.filter(
-      (n) =>
-        n.url.toLowerCase().includes(string.toLowerCase()) ||
-        n.description.toLowerCase().includes(string.toLowerCase()) ||
-        n.title.toLowerCase().includes(string.toLowerCase())
-    )
-    return results
+app.get('/results', async (req, res) => {
+  await client.connect()
+  if (!req.query.search)  { 
+    res.send([]) 
+  } else {
+    let searchResults = await Results.find( {$or: [{title: { $regex: req.query.search , $options: 'i'}}, {description: { $regex: req.query.search , $options: 'i'}}, {url: { $regex: req.query.search , $options: 'i'}}]}).toArray()
+    res.send(searchResults) 
   }
-
-
-
+})
 
 
 app.get('/', (req, res) => {
     res.send('Welcome to Google Search')
 })
 
-app.get('/results', (req, res) => {
-let searchResults = search(req.query.search)
-res.send(searchResults)
 
-}) 
-
-
-
-app.listen(4000)
+app.listen(4000, () => {
+  console.log('Listening on 4000')
+})
